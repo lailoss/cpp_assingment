845068fb58e56b25ad9579f205c76376e9a0d6f2
lailoss <feqhah01@gmail.com>
2024-12-21 20:25:13 +0800

    added fileinput file mdb and txt

--- TT4L_G01.cpp
+++ TT4L_G01.cpp
@@ -15,4 +15,73 @@
 // Member_3:
 // *********************************************************
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 
+//function prototypes
+void create_output_screen_and_file();
+void create_database();
+void create_table();
+void insert_into_table();
+void select_all_from_table_in_csv_mode();
+
+int main()
+{
+    fstream fileInput;
+
+    string fileOutputName;
+
+    //string fileInput name = "C:\\Your Name\\fileInput1.mdb";
+    //string fileInput name = "C:\\Your Name\\fileInput2.mdb";
+    //string fileInput name = "C:\\your name\\fileInput3.mdb";
+
+    fileInput.open(fileInputName);
+
+    if ( !fileInput.is_open() )
+    {
+        cout << "Unable to open file" << endl;
+        exit(-1);
+    }
+
+    string line;
+    while ( getline(fileInput, line))
+    {
+        cout << line << endl;
+
+        fileInput.close();
+
+        //fileOutputName = "fileOutput1.txt"; //incorrect
+        //cout << "> CREATE " << fileOutputName << ";" endl;
+
+        cout << "> DATABASES;" << endl;
+        cout << fileInputName << endl;
+
+        return 0;
+    }
+
+    // function definitions
+    void create_output_screen_and_file()
+    {
+
+    }
+    void create_database()
+    {
+
+    }
+    void create_table()
+    {
+
+    }
+    void insert_into_table()
+    {
+
+    }
+    void select_all_from_table_in_csv_mode()
+    {
+
+    }
+
+}
