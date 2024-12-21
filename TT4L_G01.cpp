78001bb221a2ec0635727a2cb5f16a9aae421113
lailoss <feqhah01@gmail.com>
2024-12-21 13:49:46 +0800

    added comments for our group details

--- TT4L_G01.cpp
+++ TT4L_G01.cpp
@@ -1,1 +1,18 @@
-//Start of TT4L_G01.cpp 
+// *********************************************************
+// Program: TT4L_G01.cpp
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC1L
+// Tutorial Class: TT4L
+// Trimester: 2430
+// Member_1: 242UC244FY | FEQHAH DELILAH BINTI MOHD FAIZUK | feqhah01@gmail.com | 013-662 0012
+// Member_2: 242UC244CK | WAN HANANI IMAN | EMAIL | PHONE
+// Member_3: 242UC244L8 | VIDHYA DARINEY | EMAIL | PHONE
+// *********************************************************
+// *********************************************************
+// Task Distribution
+// Member_1:
+// Member_2:
+// Member_3:
+// *********************************************************
+
+
